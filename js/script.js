31f5588d8eb7cf8186c17a62c1790030477ceee4
Andy Richardson <axrich@gmail.com>
2013-01-26 15:19:36 -0600

    started styling and such

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*$.post('get_tags.py', { username: 'andimal', limit: 3 }, function(data) {
+	var test = $.parseJSON(data);
+	$(test).each(function() {
+		$('#tags').append(this[0] + ', ' + this[1] + '<br />');
+	});
+});*/	
