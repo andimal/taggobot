c8c3597c0d8c2f275a2eb8ff5750d053c7701923
Andy Richardson <axrich@gmail.com>
2013-02-04 22:44:37 -0600

    first pages commit

---
+++ get_tags.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+print "Content-type: text/plain"
+print
+
+import urllib2, json, operator, cgi, tempfile
+# Get posted data
+form = cgi.FieldStorage()
+username = form['username'].value
+limit = form['limit'].value
+
+# Build api address
+address = 'http://api.tumblr.com/v2/blog/' + username + '.tumblr.com/posts?api_key=H5WRMruB7dtatrPairlLfPVtDEEfNtoZSfEt48GJTCxOhkn9Hg'
+
+#Query "info" on the tumblr account to get post count
+f = urllib2.urlopen(address)
+info = json.loads(f.read())
+posts = info['response']['blog']['posts']
+
+# Tumblr limits responses to 20; this gets the needed loop count
+if posts % 20 > 0:
+	loop_num = (posts / 20) + 2
+else:
+	loop_num = posts / 20 + 1
+
+# A dict for the tags
+tags = {}
+
+for x in xrange(0,loop_num):
+	
+	# Set the offset number needed to get the next set of posts
+	y = x * 20
+	# Query for the response
+	g = urllib2.urlopen(address + '&offset=' + str(y))
+
+	# Load the json
+	feed = json.loads(g.read())
+
+	# For each post...
+	for post in feed['response']['posts']:
+		# For each tag in that post...
+		for tag in post['tags']:
+			tag = tag.lower()
+			# If it's not in the tags dict yet, add it with a count of 1
+			if tag not in tags:
+				tags[tag] = 1
+			# If it is in there, increase its count
+			else:
+				tags[tag] += 1
+
+# For each tag in the dict...
+for tag in tags.keys():
+	# If the tag's count is less than the posted limit, get rid of it
+	if tags[tag] < int(limit):
+		del tags[tag]
+
+# Most-used tags first
+sorted_tags = sorted(tags.iteritems(), key=operator.itemgetter(1), reverse=True)
+# Back to json, purty indenting
+dump = json.dumps(sorted_tags, indent=4)
+
+#Set the temp file dir
+temp_dir = 'tmp/'
+# Append each temp file with "username_"
+temp_prefix = username + '_'
+# Create the temp file
+tf = tempfile.NamedTemporaryFile(
+									delete=False,
+									dir=temp_dir,
+									prefix=temp_prefix,
+									suffix='.json'
+								)
+tf_name = tf.name
+name_split = tf_name.split('/')
+tf_name = name_split[len(name_split) - 1]
+
+# Write the json to the temp file
+tf.write(dump)
+# Close(don't delete) the file
+tf.close()
+
+# Add the temp filename to the array here(so it's not in the
+# json output file), re-sort and jsonify to send to the ui
+tags['temp_filename'] = tf_name
+sorted_tags = sorted(tags.iteritems(), key=operator.itemgetter(1), reverse=True)
+dump = json.dumps(sorted_tags, indent=4)
+
+# Return the json to the ui
+print dump
+
+
---
+++ js/script.js
@@ -0,0 +1,172 @@
+$('.heading-shadow').css({
+	'left'		: 3,
+	'top'		: $('h1').offset().top + 3,
+	'display'	: 'block'
+});
+
+var drag_x = 0,
+	is_first_click = true;
+
+$('.scissors').mousedown(function () {
+	if (is_first_click) {
+		$('.tear-off').addClass('show-cut');
+		setTimeout(function () {
+			$('.tear-off').removeClass('show-cut');
+			is_first_click = false;
+		}, 200);
+	}
+});
+
+$( '.scissors').draggable({
+	axis: 'x',
+	containment: 'window',
+	start: function () {
+		$(this).find('img').addClass('cutting');
+	},
+	stop: function () {
+		$(this).find('img').removeClass('cutting');
+		if ($('.cut').width() > $('.output').width() / 2) {
+			var left_to = parseInt($('.cut').css('max-width'), 10);
+			$(this).animate({
+				left: $('.output').offset().left + $('.output').width() + 40
+			}, 1000, function() {
+				$(this).fadeOut();
+			});
+
+			$('.cut').animate({
+				width: left_to
+			}, 800, function() {
+				window.open('tmp/' + temp_file);
+				$('pre a').attr('href', 'tmp/' + temp_file);
+				$('pre a').fadeIn();
+			});
+		};
+	},
+	drag: function (event, ui) {
+		var left = $('.output').offset().left - 30;
+		$('.cut').css('max-width', $('.output').width() + 1);
+		if( ui.offset.left > left && ui.offset.left > drag_x ) {
+			$('.cut').width(ui.offset.left - left);
+			drag_x = ui.offset.left;
+		}
+	}
+});
+
+var username,
+	limit,
+	temp_file;
+
+function reset () {
+	$('table tr').each(function() {
+		if(!$(this).hasClass('title') && !$(this).hasClass('sub')) {
+			$(this).remove();
+			$('.output-top').fadeOut(0);
+		}
+	});
+	$('.scissors').removeAttr('style');
+	$('.cut').removeAttr('style');
+	drag_x = 0;
+}
+
+function post () {
+	username = $('.username').val();
+	limit = $('.limit').text();
+	$.post('get_tags.py', { username: username, limit: limit }, function(data) {
+
+		if(!data) {
+			$('.username').addClass('error');
+		} else {
+			data = $.parseJSON(data);
+
+			$('.title td').text(username.toUpperCase() + '\'S TAGS W/ ' + limit + '+');
+
+			reset();
+			if (data.length) {
+				$(data).each(function() {
+					if(this[0] === 'temp_filename') {
+						temp_file = this[1];
+					} else {
+						$('.tags .output code table').append('<tr><td>' +
+							this[0] + '</td><td class="count">' +
+							this[1] + '</td></tr>');
+					}
+				});
+			} else {
+				$('.tags .output code table').append('<tr><td class="no-tags">NO TAGS</td></tr>');
+			}
+
+			$('.tags').fadeIn('fast');
+			$('.bot .arrow').fadeIn('fast');
+			$('.bot').removeClass('bot-init');
+
+			var output_height = $('.output').height() + 20;
+			$('.output').css('top', -1 * output_height);
+			$('.output').css('margin-bottom', 20);
+			$('.output').css('visibility', 'visible');
+
+			$('html, body').animate({
+				scrollTop: $('.outputter').offset().top - 40
+			}, 'slow');
+
+			$('.output-top').fadeIn();
+
+			$('.right').animate({
+				height: 10
+			}, 'slow', function() {
+				$('.output').animate({
+					top: 0
+				}, 1000, function() {
+					$('.scissors img').addClass('cutting');
+					setTimeout(function () {
+						$('.scissors img').removeClass('cutting');
+					}, 200);
+				});
+			});
+		}
+
+		$('.go').removeClass('disabled going');
+		$('.go').text('Go!');
+	});
+}
+
+function set_limit (limit) {
+	$( '.limit' ).text( limit );
+}
+
+$('.slider').slider({
+	value	: 4,
+	min		: 1,
+	max		: 20,
+	step	: 1,
+	slide: function( event, ui ) {
+		set_limit(ui.value);
+	},
+	start: function() {
+		$('.limit').addClass('limit-active');
+	},
+	stop: function() {
+		$('.limit').removeClass('limit-active');
+	}
+});
+
+set_limit($( '.slider' ).slider( 'value' ));
+
+$('.username').keyup(function() {
+	if($(this).val() !== '') {
+		$('.go').removeClass('disabled');
+	} else {
+		$('.go').addClass('disabled');
+	}
+});
+
+$('.go').click(function() {
+	event.preventDefault();
+	if (!$(this).hasClass('disabled')) {
+		$('.username').removeClass('error');
+		$(this).addClass('disabled going');
+		$(this).text('Going...');
+		post();
+	} else {
+		$('.username').addClass('error');
+	};
+});
